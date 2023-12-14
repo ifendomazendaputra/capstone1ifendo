cc77c2559176d6538ef492e438e6a23f3435171b
ifendomazendaputra <153535025+ifendomazendaputra@users.noreply.github.com>
2023-12-14 12:32:07 +0700

    Update capstone1.py

--- capstone1.py
+++ capstone1.py
@@ -5,42 +5,46 @@ barang = read(path('barang'))
 
 clear(), print(sorotmenu(credit))
 while True:
-    # try:
-    etc()
-    pilihan = pilihmenu(['| New Cart    |', '| Open Cart   |', '| Add Stock   |',
-                            '| Exit        |'], vertical = False, header = credit1)
-    
-    if pilihan == 1:                    #new cart
-        print('Pembelian\n')
-        barang, cart = newcart(barang)
-
-        if len(cart)!= 0 and cart[-1] == 'p':
-            cart[-1] = "'" + input('\nMasukkan Nama Pembeli : ') + "'"
-            write(cart, path('carts'))
-            os.system('notepad.exe ' + path('carts'))
-            continue
+    try:
+        etc()
+        pilihan = pilihmenu(['| New Cart    |', '| Open Cart   |', '| Add Stock   |',
+                                '| Exit        |'], vertical = False, header = credit1)
         
-        if pembayaran(cart):
-            cart.append("'paid'")
-            write(cart, path('transactions'))
-            os.system('notepad.exe ' + path('transactions'))
+        if pilihan == 1:                    #new cart
+            print('Pembelian\n')
+            barang, cart = newcart(barang)
+    
+            if len(cart)!= 0 and cart[-1] == 'p':
+                cart[-1] = "'" + input('\nMasukkan Nama Pembeli : ') + "'"
+                write(cart, path('carts'), user = True)
+                os.system('notepad.exe ' + path('carts'))
+                continue
+            
+            if pembayaran(cart):
+                cart.append("'paid'")
+                write(cart, path('transactions'), user = True)
+                os.system('notepad.exe ' + path('transactions'))
+    
+                os.remove(path('barang'))
+                write(barang, path('barang'))
+                os.system('notepad.exe ' + path('barang'))
+        
+        elif pilihan == 2:                  #kelola cart
+            print('Kelola Keranjang\n')
+            # print(carts)
+            # barang, cart = kelolacart(barang, carts)
+        
+        elif pilihan == 3:                  #Tambah stock
+            print('Tambah Stock\n')
+            show([['nama', 'id', 'jenis', 'segmen', 'stok', 'harga']] + barang)
+            barang = tambahstock(barang)
 
             os.remove(path('barang'))
             write(barang, path('barang'))
             os.system('notepad.exe ' + path('barang'))
-    
-    elif pilihan == 2:                  #kelola cart
-        print('Kelola Keranjang\n')
-        # print(carts)
-        # barang, cart = kelolacart(barang, carts)
-    
-    elif pilihan == 3:                  #Tambah stock
-        print('Tambah Stock\n')
-        show([['nama', 'id', 'jenis', 'segmen', 'stok', 'harga']] + barang)
-        barang = tambahstock(barang)
-    
-    elif pilihan == 4:                  #exit
-        print('Terima Kasih')
-        break
-    else: error()
-    # except: error()
+        
+        elif pilihan == 4:                  #exit
+            print('Terima Kasih')
+            break
+        else: error()
+    except: error()
